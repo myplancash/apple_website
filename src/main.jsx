a7eacdd8c33f5f7f7e8a2b1206bb1bc51804429f
sergio esteban torres <myplancash@gmail.com>
2024-04-03 10:11:22 -0500

    created features section

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://43db3ac7037d4790e3d8fb58444fe147@o4507017621864448.ingest.us.sentry.io/4507017625403392",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.browserTracingIntegration({
+        enableInp: true,
+    }),
+    
+    /* Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }), */
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
